5b7a18e97b27d5c63c5a32e26eaa5938c46db01e
Chian-Huei Hsiao <b04902009@ntu.edu.tw>
2018-04-30 22:53:04 +0800

    change variable C to P (put option) & add note

--- hw1/hw1.cpp
+++ hw1/hw1.cpp
@@ -1,26 +1,28 @@
+/*****************************************************************************************
+Write a program to price the American put and its delta based on the CRR binomial tree.
+Inputs: (1) S (spot price), (2) X (strike price), (3) r (risk-free interest rate), 
+        (4) s (volatility), (5) T (years), (6) n (number of periods). 
+Output: put price and delta. 
+For example, assume S = 100, X = 105, r = 0.03, s = 0.25, T = 1, and n = 300. 
+Then the put price is 11.4336 and the delta is −0.5066.
+*****************************************************************************************/
 #include <iostream>
 #include <cmath>
 using namespace std;
 int main(){
     double S, X, T, s, r;
     int n;
-    printf("Spot price: ");
-    scanf("%lf", &S);
-    printf("Strike price: ");
-    scanf("%lf", &X);
-    printf("Risk-free interest rate: ");
-    scanf("%lf", &r);
-    printf("Volatility: ");
-    scanf("%lf", &s);
-    printf("Maturity in years: ");
-    scanf("%lf", &T);
-    printf("Number of periods: ");
-    scanf("%d", &n);
+    printf("Spot price: ");                 scanf("%lf", &S);
+    printf("Strike price: ");               scanf("%lf", &X);
+    printf("Risk-free interest rate: ");    scanf("%lf", &r);
+    printf("Volatility: ");                 scanf("%lf", &s);
+    printf("Maturity in years: ");          scanf("%lf", &T);
+    printf("Number of periods: ");          scanf("%d", &n);
 
     double R = exp(r * T/n);
     double u = exp(s * sqrt(T/n)), d = 1 / u;
     double p = (R-d) / (u-d);
-
+    
 // Initial values at time t
     /*
     The CRR method ensures that the tree is recombinant.
@@ -28,11 +30,11 @@ int main(){
         S_n = S * u^(N_u - N_d)
     where N_u(N_d) is the number of up(down) ticks.
     */
-    double C[n+1];
+    double P[n+1];
     for(int i = 0; i <= n; i++){
         double Sn = S * pow(u, 2*i-n);  // N_u = i, N_d = n-i => N_u - N_d = 2*i-n
-        C[i] = X - Sn;                  // The option value at each final node = Strike price - Spot price
-        if(C[i] < 0)  C[i] = 0;
+        P[i] = X - Sn;                  // The option value at each final node = Strike price - Spot price
+        if(P[i] < 0)  P[i] = 0;
     }
     
 // Move to earlier times
@@ -47,19 +49,19 @@ int main(){
     double h;   // hedge ratio
     for(int j = n-1; j >= 0; j--){              // period j
         for(int i = 0; i <= j; i++){            // node i
-            C[i] = p0 * C[i+1] + p1 * C[i];     // Binomial value C_{j,i}
+            P[i] = p0 * P[i+1] + p1 * P[i];     // Binomial value C_{j,i}
             double e = X - S * pow(u, 2*i-j);   // Exercise value
-            if(C[i] < e)  C[i] = e;             // American option: value = max(Binomial Value, Exercise Value)
+            if(P[i] < e)  P[i] = e;             // American option: value = max(Binomial Value, Exercise Value)
         }
         if(j == 1)
-            h = (C[1]-C[0])/(S*u-S*d);
+            h = (P[1]-P[0])/(S*u-S*d);
     }
-    printf("American put price: %lf\nDelta: %lf\n", C[0], h);
+    printf("American put price: %lf\nDelta: %lf\n", P[0], h);
     return 0;
 }
 
 /*
-S = 50; K = 50; r = 0.05; s = 0.2; t = 0.5; n = 100;
+S = 50; X = 50; r = 0.05; s = 0.2; T = 0.5; n = 100;
 put price: 2.3246
 delta: -0.432792
 
